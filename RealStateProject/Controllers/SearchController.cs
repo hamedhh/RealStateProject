ed1c248748c8d858dc9190656db82c3ff45f1c1d
hamedhh <hamed_71hh@yahoo.com>
2019-06-26 15:30:05 +0430

    تکمیل قسمت یو ای بخش جستجو صفحه اصلی

--- RealStateProject/Controllers/SearchController.cs
+++ RealStateProject/Controllers/SearchController.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DataLayer.DB;
+using DataLayer.ViewModels;
 
 namespace RealStateProject.Controllers
 {
     public class SearchController : Controller
     {
+        RealState_DBEntities _db = new RealState_DBEntities();
         // GET: Search
-        public ActionResult Index()
-        {
+        public ActionResult Defualt()
+       {
+            ViewBag.CountryID = new SelectList(_db.Countries.ToList(), "CountryID", "CountryTitle");
+            ViewBag.usageID = new SelectList(_db.Usages.ToList(), "UsageID", "UsageTitle");
+            ViewBag.propertyTypeID = new SelectList(_db.HomeProperty_Type.Where(a=>a.PropertyTypeID==3 || a.PropertyTypeID==2).OrderByDescending(a=>a.PropertyTypeID), "PropertyTypeID", "Title");
+            Session["Buy"] = true;
             return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Defualt(DefualtSearchViewModel _defualtSearchViewModel)
+        {
+            ViewBag.CountryID = new SelectList(_db.Countries.ToList(), "CountryID", "CountryTitle");
+            return PartialView();
+        }
+
+        public JsonResult findCity(int id)
+        {
+            var cities =_db.Cities.Where(a => a.CountryID == id).Select(c=>new {ID=c.CityID,Name=c.CityTitle}).ToList();
+            return Json(cities, JsonRequestBehavior.AllowGet);
+
         }
     }
 }
